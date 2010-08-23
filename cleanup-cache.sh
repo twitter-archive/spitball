4afb9f94bb3321e93cb2ee986714f1d4cb51c3e0
Matt Freels <matt@freels.name>
2010-08-23 12:31:34 -0700

    typo

--- cleanup-cache.sh
+++ cleanup-cache.sh
@@ -6,4 +6,4 @@ export GEM_PATH=$PREFIX/vendor
 export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PATH
 export SPITBALL_CACHE=$PREFIX/cache
 
-ruby -rubygems -Ilib bin/spitball-cleanup-cache
+ruby -rubygems -Ilib bin/spitball-cache-cleanup
