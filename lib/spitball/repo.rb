91f78d3a01d868fe3366665c0fd8240f49938689
Matt Freels <matt@freels.name>
2010-08-23 12:13:48 -0700

    add cache cleanup features

--- lib/spitball/repo.rb
+++ lib/spitball/repo.rb
@@ -9,14 +9,18 @@ module Spitball::Repo
   end
 
   def exist?(digest)
-    File.exist? path(digest, 'tgz')
+    File.exist? tarball(digest)
+  end
+
+  def tarball(digest)
+    path(digest, 'tgz')
   end
 
   def gemfile(digest)
-    File.read path(digest, 'gemfile')
+    path(digest, 'gemfile')
   end
 
-  def list_cached
+  def cached_digests
     Dir[File.join(WORKING_DIR, 'bundle_*.tgz')].map do |path|
       path.match(/bundle_(.*?)\.tgz$/)[1]
     end.compact.uniq.sort
@@ -24,5 +28,19 @@ module Spitball::Repo
 
   def make_cache_dir
     FileUtils.mkdir_p WORKING_DIR
+  end
+
+  def clean_up_unused(access_window)
+    cutoff_time = Time.now - access_window
+
+    cached_digests.each do |digest|
+      stat = File.stat(tarball(digest))
+      access_time = [stat.atime, stat.mtime].max
+
+      if access_time < cutoff_time
+        File.unlink(tarball(digest)) if File.exist? tarball(digest)
+        File.unlink(gemfile(digest)) if File.exist? gemfile(digest)
+      end
+    end
   end
 end
