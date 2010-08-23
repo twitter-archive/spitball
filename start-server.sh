701afbba720b0f5c4ac4837a235e259fa72ced9d
Matt Freels <matt@freels.name>
2010-08-23 10:43:35 -0700

    fix script, output server process PID

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-PREFIX=`readlink -f $0/..`
+PREFIX=`cd \`dirname $0\`; pwd`
+PIDFILE=$PREFIX/server.pid
 
 export GEM_PATH=$PREFIX/vendor
 export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PATH
@@ -8,4 +9,4 @@ export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PATH
 export SPITBALL_CACHE=$PREFIX/cache
 
 
-nice -n10 spitball-server -p 8080
+nice -n10 sh -c 'echo $$ > '$PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 8080'
