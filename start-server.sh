6d0240d77bd0a49dfdf90e3e688837ec54536347
Matt Freels <matt@freels.name>
2010-08-23 13:53:28 -0700

    allow pidfile specification via an env variable

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 PREFIX=`cd \`dirname $0\`; pwd`
-PIDFILE=$PREFIX/server.pid
 
 export GEM_PATH=$PREFIX/vendor
 export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PATH
-
 export SPITBALL_CACHE=$PREFIX/cache
 
+if [ "$SPITBALL_PIDFILE" = "" ]; then
+    SPITBALL_PIDFILE=$PREFIX/server.pid
+fi
 
-nice -n10 sh -c 'echo $$ > '$PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 8080'
+nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 8080'
