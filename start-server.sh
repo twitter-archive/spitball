f7e9f7fbdfb8e140a43b37b689230ca4f1148031
Matt Freels <matt@freels.name>
2010-09-02 15:50:39 -0700

    disown

--- start-server.sh
+++ start-server.sh
@@ -10,4 +10,5 @@ if [ "$SPITBALL_PIDFILE" = "" ]; then
     SPITBALL_PIDFILE=$PREFIX/server.pid
 fi
 
-nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134'
+nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134' &
+disown -h
