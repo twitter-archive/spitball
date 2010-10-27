ed96063296170757471d0060d632c3c236a39c6f
Matt Freels <matt@freels.name>
2010-10-27 15:52:15 -0700

    fix path, append to log...

--- start-server.sh
+++ start-server.sh
@@ -3,7 +3,7 @@
 PREFIX=`cd \`dirname $0\`; pwd`
 
 export GEM_PATH=$PREFIX/vendor
-export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PREFIX/../../shared/ruby/bin:$PATH
+export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PREFIX/../shared/ruby/bin:$PATH
 export SPITBALL_LOG_FILE=$PREFIX/../../shared/spitball.log
 export SPITBALL_CACHE=$PREFIX/cache
 
@@ -11,5 +11,5 @@ if [ "$SPITBALL_PIDFILE" = "" ]; then
     SPITBALL_PIDFILE=$PREFIX/server.pid
 fi
 
-nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134 > '$SPITBALL_LOG_FILE' 2>&1' &
+nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134 >> '$SPITBALL_LOG_FILE' 2>&1' &
 disown -h
