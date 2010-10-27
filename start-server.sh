5f20d79e52b8ac1d58de3704fa2b12063405c87c
Matt Freels <matt@freels.name>
2010-10-27 15:44:31 -0700

    log sinatra stderr/stdout

--- start-server.sh
+++ start-server.sh
@@ -4,11 +4,12 @@ PREFIX=`cd \`dirname $0\`; pwd`
 
 export GEM_PATH=$PREFIX/vendor
 export PATH=$PREFIX/bin:$PREFIX/vendor/bin:$PREFIX/../shared/ruby/bin:$PATH
+export SPITBALL_LOG_FILE=$PREFIX/../shared/spitball.log
 export SPITBALL_CACHE=$PREFIX/cache
 
 if [ "$SPITBALL_PIDFILE" = "" ]; then
     SPITBALL_PIDFILE=$PREFIX/server.pid
 fi
 
-nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134' &
+nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134 > '$SPITBALL_LOG_FILE' 2>&1' &
 disown -h
