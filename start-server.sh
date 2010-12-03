535e60ae980af7d002c3117a056d155ae74a973e
Josh Hull <joshbuddy@twitter.com>
2010-12-03 14:08:59 -0800

    increment port

--- start-server.sh
+++ start-server.sh
@@ -11,5 +11,5 @@ if [ "$SPITBALL_PIDFILE" = "" ]; then
     SPITBALL_PIDFILE=$PREFIX/server.pid
 fi
 
-nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1134 >> '$SPITBALL_LOG_FILE' 2>&1' &
+nice -n10 sh -c 'echo $$ > '$SPITBALL_PIDFILE'; exec ruby -rubygems -Ilib bin/spitball-server -p 1135 >> '$SPITBALL_LOG_FILE' 2>&1' &
 disown -h
