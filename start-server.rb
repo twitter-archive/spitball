b7d117e1a23fa306476967c4d09b2517a860dcb9
Josh Hull <joshbuddy@twitter.com>
2011-03-28 11:45:26 -0700

    remove custom gem pathing

--- start-server.rb
+++ start-server.rb
@@ -16,7 +16,7 @@ if File.exist?(spitball_pidfile)
   File.unlink(spitball_pidfile)
 end
 pid = fork {
-  cmd = "GEM_CMD=/opt/local/bin/gem GEM_PATH=#{prefix}/vendor PATH=#{prefix}/bin:#{prefix}/vendor/bin:#{prefix}/../shared/ruby/bin:$PATH SPITBALL_CACHE=#{shared_prefix}/cache ruby -rubygems -Ilib bin/spitball-server -p 1134 >> '#{shared_prefix}/spitball.log' 2>&1"
+  cmd = "GEM_CMD=/opt/local/bin/gem PATH=#{prefix}/bin:#{prefix}/vendor/bin:#{prefix}/../shared/ruby/bin:$PATH SPITBALL_CACHE=#{shared_prefix}/cache ruby -Ilib -rubygems bin/spitball-server -p 1134 >> '#{shared_prefix}/spitball.log' 2>&1"
   exec(cmd)
 }
 File.open(spitball_pidfile, 'w') { |f| f << pid }
